56591a402cb95d8a94918929a490d12ea39c34b2
snaderi2000 <snaderi@csus.edu>
2021-04-15 16:05:51 -0700

    brought the excel file over as a csv

--- examples_of_ctf_vs_csv/load_metadata_from_excel.py
+++ examples_of_ctf_vs_csv/load_metadata_from_excel.py
@@ -1,10 +1,22 @@
-import xlrd
+#import xlrd
 import json
+import csv 
 
-with open(metadata_file) as file:
+with open("template_ctf.json") as file:
     json_data = json.load(file)
-default_column = json_data["tableSchema"]["columnFiles"][1]
+    
+default_column = json_data["tableSchema"]["columnFiles"][0]
 
+print(default_column) 
+
+with open("GDELT_2.0_Events_Column_Labels_Header_Row_Sep2016.csv") as csv_file:
+    next(csv_file) #skip the first line since it is the header
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    for row in csv_reader:
+        print(row[1]) 
+
+
+"""
 book = xlrd.open_workbook("myfile.xls")
 sh = book.sheet_by_index(0)
 
@@ -19,3 +31,5 @@ out_json = json.dumps(json_data, indent=4, sort_keys=True)
 
 with open("newFile") as file:
     file.write(out_json)
+
+"""
