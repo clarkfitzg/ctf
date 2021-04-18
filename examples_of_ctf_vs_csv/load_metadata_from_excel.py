b53caba1323cae984f49619281d961a7ece1e991
snaderi2000 <snaderi@csus.edu>
2021-04-18 11:13:08 -0700

    the generator should be working

--- examples_of_ctf_vs_csv/load_metadata_from_excel.py
+++ examples_of_ctf_vs_csv/load_metadata_from_excel.py
@@ -7,7 +7,7 @@ with open("template_ctf.json") as file:
     
 default_column = json_data["tableSchema"]["columnFiles"][0]
 
-print(default_column) 
+#print(default_column) 
 
 arr = []
 
@@ -23,27 +23,28 @@ with open("GDELT_2.0_Events_Column_Labels_Header_Row_Sep2016.csv") as csv_file:
 book = xlrd.open_workbook("myfile.xls")
 sh = book.sheet_by_index(0)
 """
-print(json_data["tableSchema"]["columnFiles"])
+#print(json_data["tableSchema"]["columnFiles"])
 
 #print(default_column)
-empty = []
-new_column = default_column 
+
 for i in range(0,61):
-    #new_column = default_column
+    new_column = default_column
     new_column["url"] = "column" + str(i+1) + ".txt"
     new_column["datatype"]["base"] = arr[i] #sh.cell_value(2, i)
     #print(new_column)
-    #json_data["tableSchema"]["columnFiles"].append(new_column)
-    empty.append(new_column) 
+    json_data["tableSchema"]["columnFiles"].append(new_column.copy())
     #json_data["tableSchema"]["column" + str(i+1) + ".txt"] = arr[i]
 
-#print(json_data["tableSchema"]["columnFiles"])
-print(empty)
+json_data["tableSchema"]["columnFiles"].pop(0) 
 
-"""
-out_json = json.dumps(json_data, indent=4, sort_keys=True)
+#print(json_data)
 
-with open("newFile") as file:
-    file.write(out_json)
 
-"""
+
+out_json = json.dumps(json_data, indent=4)
+#print(out_json) 
+
+
+with open("newFile.json", "w") as file:
+    file.write(out_json) 
+
