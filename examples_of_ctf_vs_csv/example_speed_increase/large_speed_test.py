b5be0d93b5f0caf626afb80cc3070b9d2bda89dd
Julian Hernandez <julianofhernandez@gmail.com>
2021-05-02 16:57:00 -0700

    Pushing test updates to machine

--- examples_of_ctf_vs_csv/example_speed_increase/large_speed_test.py
+++ examples_of_ctf_vs_csv/example_speed_increase/large_speed_test.py
@@ -13,24 +13,53 @@ ctf_reader = reader(ctf_file)
 
 logging.basicConfig(filename='app.log', filemode="a")
 
-def time_col(n):
+# def time_col(n):
+#     start = time.time()
+#     for col in ctf_reader.columns[0:n]:
+#         col_name = col.index_name
+#         for item in ctf_reader[col_name]:
+#             pass
+#     end = time.time()
+#     return end-start
+
+# f = open("times.txt", "a")
+# try:
+#     for i in range(1,62):
+#         time_str = str(time_col(i))
+#         print(time_str, flush=True)
+#         f.write(time_str + "\n")
+# except:
+#     logging.error("error")
+#     print("end")
+
+# print("end")
+# logging.error("end")
+
+# get csv times
+num_columns = 0
+csv_times = []
+with open(csv_file) as csv_object:
+    reader = csv.reader(csv_object, delimiter=delimiter)
+    x = iter(reader)
+    num_columns = len(next(x))
+
+# Runs increasingly to test columns
+# for columns_to_test in range(0,2):
+for columns_to_test in range(1, num_columns+1):
     start = time.time()
-    for col in ctf_reader.columns[0:n]:
-        col_name = col.index_name
-        for item in ctf_reader[col_name]:
-            pass
+    with open(csv_file) as csv_object:
+        reader = csv.reader(csv_object)
+        # Runs on each row from the file
+        for row in reader:
+            # Loops through n times to test each row item and execute function on them
+            for index in range(0,columns_to_test):
+                try:
+                    something = int(row[index])
+                    # row_item = row[index]
+                    # # Logic for converting first row to int
+                    # function(row_item)
+                except:
+                    pass
     end = time.time()
-    return end-start
-
-f = open("times.txt", "a")
-try:
-    for i in range(1,62):
-        time_str = str(time_col(i))
-        print(time_str, flush=True)
-        f.write(time_str + "\n")
-except:
-    logging.error("error")
-    print("end")
-
-print("end")
-logging.error("end")
+    csv_times.append(end-start)
+    print(str(end-start) + "\n")
