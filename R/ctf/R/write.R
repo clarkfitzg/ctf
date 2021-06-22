e1e36d4740cfa78f7332ca2d04426529321a43d5
Clark Fitzgerald <clarkfitzg@gmail.com>
2021-06-22 13:03:17 -0700

    quick start vignette

--- R/ctf/R/write.R
+++ R/ctf/R/write.R
@@ -6,6 +6,9 @@
 #' @param dir path to write CTF data
 #' @param ... further arguments to \code{\link[base]{cat}}
 #' @return \code{NULL}, used for its side effect
+#' @seealso \code{\link{read.ctf}} to read CTF, \code{\link[base]{cat}} for the underlying functionality, and \code{\link[base]{save}} for writing any R objects.
 #' @export
+#' @examples
+#' d <- tempdir
 write.ctf = function(x, dir, ...)
     NULL
