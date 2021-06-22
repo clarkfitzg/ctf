986228b693242362b1f48ad045a5325cf5a4aaf2
Clark Fitzgerald <clarkfitzg@gmail.com>
2021-06-22 11:40:54 -0700

    follow base R naming convention for with . rather than _

--- R/ctf/R/read.R
+++ R/ctf/R/read.R
@@ -1,11 +1,11 @@
 #' Read CTF data
 #' 
-#' Read external CTF data into the corresponding R object.
+#' Read external CTF data into the corresponding R object
 #' 
 #' @param dir path to a directory containing CTF data
 #' @param columns path to a directory containing CTF data
-#' @param ... further arguments to \code{scan}
-#' @return data frame containing of ctf data
+#' @param ... further arguments to \code{\link[base]{scan}}
+#' @return data frame of loaded data
 #' @export
-read_ctf = function(dir, columns = NULL, ...) 
+read.ctf = function(dir, columns = NULL, ...) 
     NULL
--- R/ctf/R/write.R
+++ R/ctf/R/write.R
@@ -1,2 +1,11 @@
-write_ctf <-
-function(...) NULL
+#' Write Data Frame To CTF
+#' 
+#' Save a data frame using Column Text Format
+#' 
+#' @param x data frame to write
+#' @param dir path to write CTF data
+#' @param ... further arguments to \code{\link[base]{cat}}
+#' @return \code{NULL}, used for its side effect
+#' @export
+write.ctf = function(x, dir, ...)
+    NULL
