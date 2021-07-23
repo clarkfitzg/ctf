678e55f782cf7114a08b1bda70ff3038ff2d38de
Julian Hernandez <julian.hernandez@intel.com>
2021-07-23 10:43:29 -0700

    Added writer that can add 1 value at a time

--- column_text_format/writer.py
+++ column_text_format/writer.py
@@ -1,5 +1,6 @@
-import os
-from .file_management import list_files, open_iterator, full_file
+import os, json
+from .column import Column
+from .file_management import list_files, get_file_name
 
 class Writer:
     '''This class will be used to read CTF files'''
@@ -7,19 +8,22 @@ class Writer:
     def __init__(self, file_path, bucket_name=None):
         self.file_path = file_path
         self.file_name = os.path.basename(self.file_path)
-        self.columns = []
+        self.column_file_objects = {} # Stores the file_objects for each column
+        # self.columns = []
         self.data_types = {}
-        self.bucket_name = bucket_name
-        self.file_name = os.path.basename(self.file_path)
-        self.column_files = list_files(self.file_path, bucket_name)
         # self.read_metadata()
-        for column_file in self.column_files:
-            self.columns.append(iter(Column(column_file, bucket_name=self.bucket_name)))
+        for column_file in list_files(self.file_path, bucket_name):
+            print(column_file)
+            key = get_file_name(column_file)
+            print(key)
+            self.column_file_objects[key] = open(column_file, "a")
 
+    def write(self, data):
+        # Checks to see if we are writing a list of values
+        for key in data:
+            self.column_file_objects[key].write(str(data[key]) + '\n')
+            self.column_file_objects[key].close()
 
-    def write(self, write_dict):
-        print(write_dict)
-
-
-
-    
+    def close(self):
+        for key in self.column_file_objects:
+            self.column_file_objects[key].close()
