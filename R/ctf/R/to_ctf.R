d3e6dd250dbd016e877518bdff5e7c2d4a2081e1
alihyung <alisa2001zaa@gmail.com>
2024-07-12 13:09:00 -0700

    added cases to write.ctf and made chunks

--- R/ctf/R/to_ctf.R
+++ R/ctf/R/to_ctf.R
@@ -3,6 +3,7 @@
 #' Direct conversion from CSV file to CTF data.
 #'
 #' @param csvpath a CSV file or file path
+#' @param datadir 
 #' @param ... further arguments to \code{\link[ctf]{write.ctf}}
 #'
 #' @return a CTF directory
@@ -13,16 +14,24 @@
 #' d <- "inst/extdata/vgsales.csv"
 #' 
 #' # Writes a CTF directory in the working directory
-#' to_ctf(d)
+#' to_ctf(d, "vgsales")
 #' 
 #' # A new directory contains plain text files for each column in vgsales
 #' list.files("vgsales")
 #' 
 #' Clean up
 #' unlink("vgsales", recursive = TRUE)
-to_ctf = function(csvpath, ...)
+to_ctf = function(csvpath, datadir, ...)
 {
-	  df = read.csv(csvpath)
-  directory_name = tools::file_path_sans_ext(basename(csvpath))
-    write.ctf(df, directory_name, name = directory_name, ...)
+	datadir = basename(datadir)
+	cutdf = iotools::read.csv.raw(csvpath, header = FALSE, nrows = 1)
+	col_names = unlist(split(cutdf, seq(nrow(cutdf))))
+	iotools::chunk.apply(csvpath, 
+			     function(x){
+				df = iotools::read.csv.raw(x)
+				colnames(df) = col_names
+				write.ctf(df, datadir, name = datadir, appendRows = TRUE, ...)
+				}, 
+				CH.MAX.SIZE = 2e5)
+      	invisible(NULL)
 }
