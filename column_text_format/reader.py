2fb0802e4d8ef8d269be33c510fe11deaacd9689
Saeknir <dylan.a.kunz@gmail.com>
2024-07-15 10:29:48 -0700

    initial completion of python reader

--- column_text_format/reader.py
+++ column_text_format/reader.py
@@ -4,10 +4,11 @@ import time
 import glob
 import json
 import pandas as pd
-from .column import Column
-from .file_management import list_files, open_iterator, full_file
 
 META_TYPE = "json"
+NEWLINE = "\n"
+FILETYPE = ".txt"
+CONTEXT = ["http://www.w3.org/ns/csvw"]
 """Legacy code. Kept temporarily as context.
 class Reader:
     '''
@@ -177,10 +178,28 @@ def stream_convert_csv_to_ctf(stream, path, name, **kwargs):
             ctf_file.write(value + "\n")
             #ctf_file.writeline(value) ??
 """
-def findMeta(location)
+
+def create(datasetName, location):
+    colCounter = 0
+    #TODO: check if legit dataset
+    dataLoc = 'https://raw.githubusercontent.com/mwaskom/seaborn-data/master/'+datasetName+'.csv'
+    dataset = pd.read_csv(dataLoc)
+    tableArray = []
+    colArray = []
+    for colName, col in dataset.items():
+        colArray.append({'url': colName, 'titles': colName, 'datatype': str(dataset.dtypes[colName].name)})
+        with open(colName+FILETYPE, 'w') as colWrite:
+            for value in col:
+                colWrite.write(str(value)+"\n")
+    tableArray.append({'url':[datasetName+'.csv'], 'tableSchema':{'columns':colArray}})
+    jsonDictionary = { '@context': CONTEXT, 'tables':tableArray}
+    with open(datasetName+'.'+META_TYPE, 'w') as file:
+        json.dump(jsonDictionary, file, ensure_ascii=False, indent=2)
+
+def findMeta(location):
     metafile = None
     #Iterate through the files in the location.
-    for root, dirs, files in os.walk(."location"):
+    for root, dirs, files in os.walk(location):
         #check if any have the type used for metadata.
         for name in files:
             #Find the first file of the type used for metadata. Upon finding it, assign it to metafile and break.
@@ -189,16 +208,15 @@ def findMeta(location)
                 break
     #If the directory doesn't have a metafile, throw an error and break.
     #Check how we want to do error logging
-    if(metafile == None)
+    #if(metafile == None):
         #TODO Print Error
     return metafile
-        
 
 #Read a ctf metadata file to create a dataframe. It will read nrows rows from each column file. 
-def read(location, columns, nrows)
+def read(location, columns, numRows):
     metafile = findMeta(location)
     dataframe = None
-    if(metafile != None)
+    if(metafile != None):
         file = open(metafile, "r")
         metadata = json.loads(file.read())
         for col in columns:
@@ -207,27 +225,17 @@ def read(location, columns, nrows)
             columnActual = None
             if isinstance(col, int):
                 columnActual = col
-            else 
-                for columnID, metaColumns in enumerate(metadata['tableSchema'])
+            else:
+                for columnID, metaColumns in enumerate(metadata['tables'][0]['tableSchema']['columns']):
                     if(metaColumns['titles']) == col:
                         columnActual=columnID
-            colName = metadata['tableSchema'][columnActual]
-            colLocation = metadata['tableSchema'][columnActual]['url']
-            colFile = open(colLocation, "r")
-            head = [next(colFile) for _ in range(nrows)]
-            row = {colName:head}
-            if dataframe:
-                dataframe.append(row)
-            else:
-                dataframe = pd.DataFrame = (row)
+            if(columnActual != None):
+                colLocation = metadata['tables'][0]['tableSchema']['columns'][columnActual]['url']
+                colName = metadata['tables'][0]['tableSchema']['columns'][columnActual]['titles']
+                readColumn = pd.read_csv(colLocation+".txt", nrows=numRows)
+                readColumn.rename(columns={readColumn.columns[0]:colName}, inplace=True)
+                if dataframe is not None:
+                    dataframe = pd.concat([dataframe, readColumn], axis=1)
+                else:
+                    dataframe = readColumn
     return dataframe
-
-#If the number of rows isn't specified, grab the number of rows from metadata and select that many. Should grab all rows if metadata is sound.
-def read(location, columns)
-    metafile = findMeta(location)
-    if(metafile != None)
-        file = open(metafile, "r")
-        metadata = json.loads(file.read())
-        #TODO if!metadata[rowCount]
-        read(location, columns, metadata[rowCount])
-    
